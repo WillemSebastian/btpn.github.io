da6ce11a7a5308b8e3e33d3e9e2cc316293c6e04
willem Sebastian <willem1991@yahoo.co.id>
2020-03-01 21:19:59 +0700

    Updates

---
+++ precache-manifest.e32f3a07a68ab7564450a75c27711efa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898832759647ec3ab335722cd10af2f2",
+    "url": "/btpn.github.io/index.html"
+  },
+  {
+    "revision": "99cdbee43c3d37217184",
+    "url": "/btpn.github.io/static/css/main.0fce6d35.chunk.css"
+  },
+  {
+    "revision": "e3e75cd5d8ec892bbacd",
+    "url": "/btpn.github.io/static/js/2.ad91ca90.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/btpn.github.io/static/js/2.ad91ca90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99cdbee43c3d37217184",
+    "url": "/btpn.github.io/static/js/main.2b744584.chunk.js"
+  },
+  {
+    "revision": "52b9a06a53554b34dc70",
+    "url": "/btpn.github.io/static/js/runtime-main.3b700e73.js"
+  }
+]);
